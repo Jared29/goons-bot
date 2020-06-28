b12b91adaacd52766074000bfa6e3f7239573a33
Jared Stull <jstull29@hotmail.com>
2020-06-27 21:48:51 -0500

    pls work

--- src/commands/zoe.js
+++ src/commands/zoe.js
@@ -1,4 +1,4 @@
 module.exports.run = async(client, message, args) => {
-    let resiSleeper = guild.emojis.get("620140834599337987");
+    let resiSleeper = guild.emojis.find(emoji => emoji.name === "residentsleeper");
     message.channel.send(`${resiSleeper} Dean ${resiSleeper} popping ${resiSleeper} off ${resiSleeper} on ${resiSleeper} Zoe ${resiSleeper} again ${resiSleeper}`);
 }
