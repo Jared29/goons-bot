db831d965b753cbf9d8b3b4dc84b381348705c68
Jared Stull <jstull29@hotmail.com>
2020-06-27 21:53:32 -0500

    get guild pls

--- src/commands/test.js
+++ src/commands/test.js
@@ -1,4 +1,4 @@
 module.exports.run = async(client, message, args) => {
-    let unon = guild.emojis.find(emoji => emoji.name === "unon");
+    let unon = message.guild.emojis.find(emoji => emoji.name === "unon");
     message.channel.send(`${unon} Dean ${unon} popping ${unon} off ${unon} on ${unon} Zoe ${unon} again ${unon}`);
 }
