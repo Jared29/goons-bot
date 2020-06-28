c06a2fff567c9c4e4b2c4f828de8f3a93fc814cb
Jared Stull <jstull29@hotmail.com>
2020-06-27 21:40:56 -0500

    zoe

---
+++ src/commands/zoe.js
@@ -0,0 +1,4 @@
+module.exports.run = async(client, message, args) => {
+    let resiSleeper = guild.emojis.get("620140834599337987");
+    message.channel.send(`${ayy} Dean ${ayy} popping ${ayy} off ${ayy} on ${ayy} Zoe ${ayy} again ${ayy}`);
+}
